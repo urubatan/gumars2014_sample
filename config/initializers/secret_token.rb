5ff7e33803dee11faccadfda63946ad295d99042
Rodrigo Urubatan <rodrigo@urubatan.com.br>
2014-04-03 21:00:07 -0300

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guma2014::Application.config.secret_key_base = '672e06190c2f80edc2a8b91702fbbc260e5372f2b23ad7d874aefd2063f6ca6a13de5cbbd1ed726bedac9c270d4ebf85846b43262f9bb7c2be6275ea09f4199f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Guma2014::Application.config.session_store :cookie_store, key: '_guma2014_session'
